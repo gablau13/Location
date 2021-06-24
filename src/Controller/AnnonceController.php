7fc9effd4bd5a46f31d0a55448b2d59506082e30
gablau13 <gablau2406@gmail.com>
2021-06-24 12:34:52 +0000

    affichage de la base de données

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Anounce;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AnnonceController extends AbstractController
 {
     #[Route('/annonce', name: 'annonce')]
     public function index(): Response
     {
+
+        // Afficher tous les annonces
+        $anounce= $this->getDoctrine()->getRepository(Anounce::class)->findAll();
         return $this->render('annonce/index.html.twig', [
-            'controller_name' => 'AnnonceController',
+            'anounce' => $anounce,
         ]);
     }
 }
+
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,14 +17,14 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
         $slugger = new Slugify();
-        for ($i = 0; $i < 5; $i++) {
+        for ($i = 0; $i < 8; $i++) {
             $anounce = new Anounce();
             $anounce->setTitle($faker->sentence(3, false))
                 ->setSlug($slugger->Slugify($anounce->getTitle()))
                 ->setdescription($faker->text(200))
                 ->setPrice(mt_rand(30000, 60000))
                 ->setAddress($faker->address())
-                ->setCoverImage("https://via.placeholder.com/500×300")
+                ->setCoverImage('https://picsum.photos/350/300/?random=' . mt_rand(1, 50000))
                 ->setRoom(mt_rand(0, 5))
                 ->setIsAvailable(mt_rand(0, 1))
                 ->setCreateAt($faker->datetimeBetween('-3 month', 'now'));
@@ -41,7 +41,7 @@ class AppFixtures extends Fixture
             }
            for ($k=0; $k<mt_rand(0,4); $k++){
                $image=new Image();
-               $image->setImageUrl('https://picsum.photos/750/500' . mt_rand(1, 50000))
+               $image->setImageUrl('https://picsum.photos/500/300/?ramdom=' . mt_rand(1, 50000))
                      ->setDescription($faker->sentence())
                      ->setAnounce($anounce);
                 $manager->persist($image);
